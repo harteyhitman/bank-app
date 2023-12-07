024e1f6d211c74111635815516fd47b25dffd6d7
harteyhitman <oyegoke.a18@gmail.com>
2023-12-07 14:00:12 +0100

    app-name

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ styles }) => {
     <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins 
     font-medium text-[18px] text-primary 
     outline-none rounded-[10px] ${styles} hover:animate-pulse hover:text-white`}
-    
+      
     >
       Get Started
     </button>
