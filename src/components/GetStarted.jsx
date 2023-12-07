dd53402b0a5e908142f6efb28713c55a77c2db34
harteyhitman <oyegoke.a18@gmail.com>
2023-12-07 12:49:43 +0100

    animation

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,7 +4,7 @@ const GetStarted = () => {
   return (
     <div className={`${styles.flexCenter}
     w-[140px] h-[140px] rounded-full bg-blue-gradient
-    p-[2px] cursor-pointer
+    p-[2px] cursor-pointer animate-spin
     `}>
       <div className={`${styles.flexCenter} 
       flex-col bg-primary w-[100%] h-[100%] rounded-full
